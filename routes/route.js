bf72c0f12840e24caca104eec14968b2b5395071
omkar vashistha <33715591+omkarvashistha@users.noreply.github.com>
2023-03-20 20:48:34 +0530

    changes to cors

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const cors = require('cors');
 
-router.use(cors);
+router.use(cors());
 
 const weeklyController = require('../Controller/weeklyController');
 
